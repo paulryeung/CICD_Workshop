aa575962775d5603dbeb83c4d9a1ac7134e8b3d1
paul.yeung <paul.yeung@ontariohealth.ca>
2026-01-15 19:18:09 +0000

    reverted text

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>PULL REQUEST TEST</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
