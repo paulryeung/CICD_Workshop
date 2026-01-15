2d9dd0a7759316043da1432be0298f74dedc28fb
paul.yeung <paul.yeung@ontariohealth.ca>
2026-01-15 19:05:39 +0000

    Initial Commit

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,11 @@
+
+import * as cdk from 'aws-cdk-lib/core';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+
+const app = new cdk.App();
+
+const testCdkStack = new AppCdkStack(app, 'test', {});
+
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
+
