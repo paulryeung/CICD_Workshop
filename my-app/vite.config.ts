0e1728cf55252466139c9019bf3f933608e3e5e0
paul.yeung <paul.yeung@ontariohealth.ca>
2026-01-15 19:53:02 +0000

    implemted CI pipeline

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types='vite/client' />
-
+/// <reference types='vitest' />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,8 +8,11 @@ export default defineConfig({
   plugins: [react()],
   base: '/my-app',
   server: {
-    allowedHosts: true,
     host: true,
     port: 8081,
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },  
 })
